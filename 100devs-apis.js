8ee9e1292e1a7090542f8bff30d8864f0d5ba684
oliver-wahome <olivernderitu135@gmail.com>
2023-01-02 23:01:53 +0300

    feat: added cocktaildb api testing section

--- 100devs-apis.js
+++ 100devs-apis.js
@@ -1,13 +1,31 @@
-fetch("https://dog.ceo/api/breeds/image/random")
+//fetching dog picture data
+function getDogImage(){
+    fetch("https://dog.ceo/api/breeds/image/random")
+        .then(res => res.json())
+        .then(data => {
+            console.log(data);
+            document.getElementById("outputImg").src = data.message;
+        })
+        .catch(err => {
+            console.log(`error ${err}`);
+        });
+}
+
+getDogImage();
+
+//function to reload page onclick of button
+function reloadPage(){
+    getDogImage();
+}
+
+//The user will enter a cocktail. Get a cocktail name, photo and
+//instructions and place them in the DOM
+const url = 'https://www.thecocktaildb.com/api/json/v1/1/search.php?s=margarita';
+fetch(url)
     .then(res => res.json())
     .then(data => {
-        console.log(data);
-        document.getElementById("outputImg").src = data.message;
+        console.log(data.drinks[0]);
     })
     .catch(err => {
         console.log(`error ${err}`);
     });
-
-function reloadPage(){
-    window.location.reload();
-}
