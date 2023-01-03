fb99710cdbbf96c9105d5a5b055412402378cd54
oliver-wahome <olivernderitu135@gmail.com>
2023-01-03 03:43:57 +0300

    feat: printing full list of drinks from search

--- 100devs-apis.js
+++ 100devs-apis.js
@@ -1,3 +1,6 @@
+/*
+    DOG API JAVASCRIPT CODE
+*/
 //fetching dog picture data
 function getDogImage(){
     fetch("https://dog.ceo/api/breeds/image/random")
@@ -10,25 +13,50 @@ function getDogImage(){
             console.log(`error ${err}`);
         });
 }
-
 getDogImage();
 
 //function to reload page onclick of button
 function reloadPage(){
     getDogImage();
 }
+// END //
 
-//The user will enter a cocktail. Get a cocktail name, photo and
-//instructions and place them in the DOM
-const url = 'https://www.thecocktaildb.com/api/json/v1/1/search.php?s=margarita';
-fetch(url)
-    .then(res => res.json())
-    .then(data => {
-        console.log(data.drinks[0]);
-        document.getElementById("drinkName").innerText = data.drinks[0].strDrink;
-        document.getElementById("drinkImg").src = data.drinks[0].strDrinkThumb;
-        document.getElementById("drinkInstructions").innerText = data.drinks[0].strInstructions;
-    })
-    .catch(err => {
-        console.log(`error ${err}`);
-    });
+/*
+    THE COCKTAIL DB AP JAVASCRIPT CODE
+*/
+//The user will enter a cocktail. Get a cocktail name, photo and instructions and place them in the DOM
+document.getElementById("submitDrinkBtn").addEventListener('click', getDrink);
+
+//function to get drink from input and fetch data from cocktaildb api
+function getDrink(){
+    let drink = document.getElementById("drinkInput").value;
+    let url = `https://www.thecocktaildb.com/api/json/v1/1/search.php?s=${drink}`;
+    document.getElementById("drinkRow").innerHTML = '';
+
+    fetch(url)
+        .then(res => res.json())
+        .then(data => {
+            console.log(data.drinks);
+
+            data.drinks.forEach(drink => {
+                var content = '';
+                console.log(drink.strDrink);
+
+                content += '<div class="row w-75 border rounded py-2 mb-3 bg-light">';
+                    content += '<div class="col-md-6 d-flex flex-column align-items-center">';
+                        content += '<h5 id="drinkName">'+drink.strDrink+'</h5>';
+                        content += '<img class="my-3 rounded" style="object-fit:cover; height:200px; width:200px;" src="'+drink.strDrinkThumb+'" alt="drink-image" id="drinkImg" />';
+                    content += '</div>';
+                    content += '<div class="col-md-6 d-flex align-items-center justify-content-center">';
+                        content += '<p class="col-9 text-center" id="drinkInstructions">'+drink.strInstructions+'</p>';
+                    content += '</div>';
+                content += '</div>';
+
+                document.getElementById("drinkRow").innerHTML += content;
+            });
+        })
+        .catch(err => {
+            console.log(`error ${err}`);
+        });
+}
+// END  //
