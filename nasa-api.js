1e7ebca2ebdac7fbb69d301832e972630c8dffb2
oliver-wahome <olivernderitu135@gmail.com>
2023-01-04 12:26:56 +0300

    update: refactoring nasa-api.js code

--- nasa-api.js
+++ nasa-api.js
@@ -1,12 +1,22 @@
-fetch("https://api.nasa.gov/planetary/apod?api_key=GTgDxMg6JEfbQPwYdlLPfygL8XDmEVWp8HWkzNnm")
-    .then(res => res.json())
-    .then(data => {
-        console.log(data);
+document.getElementById("dateInputBtn").addEventListener('click', getApod);
 
-        document.getElementById("apodImage").src = data.hdurl;
-        document.getElementById("apodName").innerText = data.title;
-        document.getElementById("apodDescription").innerText = data.explanation;
-    })
-    .catch(err => {
-        console.log(`err ${err}`);
-    })
+function getApod(){
+
+    const apodDate = document.getElementById("dateInput").value;
+    console.log(choice);
+
+    const url = 'https://api.nasa.gov/planetary/apod?api_key=GTgDxMg6JEfbQPwYdlLPfygL8XDmEVWp8HWkzNnm';
+
+    fetch(url)
+        .then(res => res.json())
+        .then(data => {
+            console.log(data);
+
+            document.getElementById("apodImage").src = data.hdurl;
+            document.getElementById("apodName").innerText = data.title;
+            document.getElementById("apodDescription").innerText = data.explanation;
+        })
+        .catch(err => {
+            console.log(`err ${err}`);
+        })
+}
